2a6aa7a97133b0e406f7c4e0863440cc510e85ef
Neeshu Kumar <neeshu.kumar@simplyentertainingapps.com>
2024-09-10 11:27:22 +0530

    Initial Commit

---
+++ In-AppPurchaseDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-AppPurchaseDemo
+//
+//  Created by Neeshu Kumar on 10/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-AppPurchaseDemo/In_AppPurchaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_AppPurchaseDemoApp.swift
+//  In-AppPurchaseDemo
+//
+//  Created by Neeshu Kumar on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct In_AppPurchaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
